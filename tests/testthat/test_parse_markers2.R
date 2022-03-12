2d2fe096605330884a45f99bc2bc0952e047394d
Dieter Menne <dieter.menne@menne-biomed.de>
2022-03-12 20:21:34 +0100

    use skip_on_ci

--- tests/testthat/test_parse_markers2.R
+++ tests/testthat/test_parse_markers2.R
@@ -2,14 +2,16 @@ Sys.setenv("R_CONFIG_ACTIVE" = "test")
 g = globals()
 withr::defer(cleanup_test_data())
 
+# These test fail on github actions by crashing the system
+# without error message
 test_that("parse_markers handles raises error without #", {
-  skip("parse")
+  skip_on_ci()
   hr_lines = c("Annotations:", "\t40.00\thusten")
   expect_error(parse_markers(hr_lines), "No valid")
 })
 
 test_that("parse_markers errors on invalid", {
-  skip("parse")
+  skip_on_ci()
   hr_lines = c("Annotations:", "\t-1.0  #B7",
                "\t-1.0  #13", "20.00 husten", "40.00\t # Rest")
   expect_error(parse_markers(hr_lines), "Invalid channel")
@@ -17,14 +19,14 @@ test_that("parse_markers errors on invalid", {
 
 
 test_that("parse_markers handles missing channels", {
-
+  skip_on_ci()
   hr_lines = c("Annotations:", "\t-1.0  # B1",
                "\t-1.0  #B2", "20.00 husten", "40.00\t # Rest")
-#  pm = parse_markers(hr_lines)
-#  expect_equal(pm$invalid_channels, c("B1", "B2"))
-#  expect_equal(pm$markers$sec, c(0, 40))
-#  expect_equal(pm$markers$annotation,  c("begin", "Rest"))
-#  expect_equal(nrow(pm$unused_markers), 1)
-#  expect_equal(pm$unused_markers$sec, 20)
-#  expect_equal(pm$unused_markers$annotation, "husten")
+  pm = parse_markers(hr_lines)
+  expect_equal(pm$invalid_channels, c("B1", "B2"))
+  expect_equal(pm$markers$sec, c(0, 40))
+  expect_equal(pm$markers$annotation,  c("begin", "Rest"))
+  expect_equal(nrow(pm$unused_markers), 1)
+  expect_equal(pm$unused_markers$sec, 20)
+  expect_equal(pm$unused_markers$annotation, "husten")
 })
