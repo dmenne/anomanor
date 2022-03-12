d4b093b1ede8b696680e89fca39f7fe2802ae8a9
Dieter Menne <dieter.menne@menne-biomed.de>
2022-03-12 18:21:04 +0100

    moved parse_markers errors up

--- tests/testthat/test_parse_markers.R
+++ tests/testthat/test_parse_markers.R
@@ -30,6 +30,12 @@ test_that("parse_markers handles regular tabs with spaces", {
   expect_equal(nrow(pm$unused_markers), 0)
 })
 
+test_that("parse_markers errors on invalid", {
+  hr_lines = c("Annotations:", "\t-1.0  #B7",
+               "\t-1.0  #13", "20.00 husten", "40.00\t # Rest")
+  expect_error(parse_markers(hr_lines), "Invalid channel")
+})
+
 test_that("parse_markers handles raises error without #", {
   hr_lines = c("Annotations:", "\t40.00\thusten")
   expect_error(parse_markers(hr_lines), "No valid")
@@ -57,13 +63,6 @@ test_that("parse_markers handles multiple rows with spaces", {
   expect_equal(pm$unused_markers$annotation, "husten")
 })
 
-test_that("parse_markers errors on invalid", {
-  skip("skip")
-  hr_lines = c("Annotations:", "\t-1.0  #B7",
-               "\t-1.0  #13", "20.00 husten", "40.00\t # Rest")
-  expect_error(parse_markers(hr_lines), "Invalid channel")
-})
-
 test_that("parse_markers handles multiple tabs", {
   skip("skip")
   hr_lines = c("Annotations:", "\t\t20.00\thusten","\t40.00\t# Rest")
@@ -77,6 +76,7 @@ test_that("parse_markers handles multiple tabs", {
 })
 
 test_that("parse_markers handles missing channels", {
+  skip("skip")
   hr_lines = c("Annotations:", "\t-1.0  # B1",
                "\t-1.0  #B2", "20.00 husten", "40.00\t # Rest")
   pm = parse_markers(hr_lines)
