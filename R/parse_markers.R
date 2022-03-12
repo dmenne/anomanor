273eb2ff7c0dac824509663b09437ef54c57965b
Dieter Menne <dieter.menne@menne-biomed.de>
2022-03-12 16:23:27 +0100

    closed textconnection in parse_markers

--- R/parse_markers.R
+++ R/parse_markers.R
@@ -1,37 +1,39 @@
 parse_markers = function(hr_lines, annot_line = 1, file = "testfile") {
   # Find annotations
-  if (length(annot_line) == 0) 
+  if (length(annot_line) == 0)
     log_stop("There are no annotations in this file")
   # https://community.rstudio.com/t/readr-include-spec-false/60787
   hr_markers = str_trim(hr_lines[(annot_line + 1):length(hr_lines)])
-  # Replace first space after number 
+  # Replace first space after number
   hr_markers = str_replace(hr_markers, "^([-\\d\\.]+) ", "\\1\t")
   # Only keep markers with a hash #
   hr_unused_markers = hr_markers[!str_detect(hr_markers, "#")]
-  unused_markers = read.delim(textConnection(hr_unused_markers), header = FALSE,  
-                              col.names =  c("sec", "annotation")) %>% 
-    as_tibble() %>% 
-    mutate(annotation = str_trim(.data$annotation)) 
+  zz = textConnection(hr_unused_markers)
+  unused_markers = read.delim(zz, header = FALSE,
+                              col.names =  c("sec", "annotation")) %>%
+    as_tibble() %>%
+    mutate(annotation = str_trim(.data$annotation))
+  close(zz)
   hr_markers = hr_markers[str_detect(hr_markers, "#")]
   # And remove the hashes
   hr_markers = str_replace(hr_markers, "# *", "")
   if (length(hr_markers) == 0)
     log_stop("No valid markers in file")
-  
-  markers = read.delim(textConnection(hr_markers), header = FALSE,  
-                       col.names =  c("sec", "annotation")) %>% 
+  zz = textConnection(hr_markers)
+  markers = read.delim(zz, header = FALSE,
+                       col.names =  c("sec", "annotation")) %>%
     mutate(annotation = str_trim(.data$annotation))
-  
-  invalid_channels = markers %>% 
-    filter(.data$sec < 0) %>% 
-    pluck("annotation") 
+  close(zz)
+  invalid_channels = markers %>%
+    filter(.data$sec < 0) %>%
+    pluck("annotation")
   allowed_invalid_channels = c("B1", "B2", paste(1:10))
   if (!is.null(invalid_channels) && !invalid_channels %in% allowed_invalid_channels)
     log_stop(
       glue("Invalid channel(s) {paste(invalid_channels, collapse = ', ')}.",
            " Only {paste(allowed_invalid_channels, collapse = ', ')} are permitted.")
     )
-  markers = markers %>% 
+  markers = markers %>%
     filter(.data$sec >= 0)
   list(
     markers = bind_rows(tribble(~sec, ~annotation, 0, "begin"),
