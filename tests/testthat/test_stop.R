b98e319173c22a45d1f38be93e25dd493fd0dad9
Dieter Menne <dieter.menne@menne-biomed.de>
2022-03-14 09:20:38 +0100

    Imported globals, no cleanup

--- tests/testthat/test_stop.R
+++ tests/testthat/test_stop.R
@@ -1,3 +1,5 @@
+Sys.setenv("R_CONFIG_ACTIVE" = "test")
+g = globals()
 test_that("expect_error works on gi actions", {
   a = try(stop("Hallo, welt", call. = FALSE))
   expect_s3_class(a, "try-error")
