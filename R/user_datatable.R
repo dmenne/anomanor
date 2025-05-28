ea754a59a046f6a4d1cec04d8f61f1cd220960f1
Dieter Menne <dieter.menne@menne-biomed.de>
2025-05-28 15:02:29 +0200

    Restored user_datatable from test

---
+++ R/user_datatable.R
@@ -0,0 +1,18 @@
+user_datatable = function(stat) {
+  ano_options = list(
+    paging = FALSE,
+    searching = FALSE,
+    autoWidth = FALSE,
+    dom = 'Bfrtip',
+    buttons = c('copy', 'excel', 'pdf')
+  )
+  DT::datatable(
+    stat,
+    rownames = FALSE,
+    extensions  = 'Buttons',
+    filter = "none",
+    options = ano_options
+  ) %>%
+  DT::formatStyle('verified',
+    color = DT::styleEqual(c(FALSE, TRUE), c("red", "")))
+}
